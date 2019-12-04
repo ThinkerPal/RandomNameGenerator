02d4fe3186630d0342aec6dfb81a73658980b390
Thinker Pal <thinkerpal@gmail.com>
2019-12-05 02:05:54 +0800

    Initial Commit

---
+++ randomName.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	string names[200], b;
+	int i=0;
+	while (getline(cin,b) && b != "!"){
+		names[i] = b;
+		i++;
+	}
+	int nu = i, ran;
+	cout<<"All Names Saved"<<endl;
+	srand(time(NULL));
+	char a;
+	system("clear");
+	cout<<"Press any key to continue...";
+	while (cin>>a && a != 'N'){
+		ran = rand() % nu;
+		cout<<names[ran]<<endl;
+		cout<<"Do you want another name? (Y/N) ";
+	}
+	return 0;
+}
